f3ec42134eb0add236b633964cace8915404f2e5
Viacheslav Azarov <public@religiosa.ru>
2024-11-18 21:37:57 +0100

    Add CLI flag to dusplay current version

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/alecthomas/kong"
 	"github.com/religiosa1/git-webhook-receiver/internal/cmd"
@@ -11,6 +12,7 @@ import (
 
 var CLI struct {
 	ConfigPath    string                `short:"c" help:"Configuration file name"`
+	Version       bool                  `short:"v" help:"Show version information and exit"`
 	Serve         struct{}              `cmd:"" default:"1" help:"Run the webhook receiver server (default mode)"`
 	Pipeline      cmd.PipelineArgs      `cmd:"" aliases:"pl,get" help:"Display pipeline output"`
 	ListPipelines cmd.ListPipelinesArgs `cmd:"" aliases:"ls" help:"Display a list of last N pipelines"`
@@ -19,6 +21,11 @@ var CLI struct {
 
 func main() {
 	args := kong.Parse(&CLI)
+
+	if CLI.Version {
+		showVersion()
+		return
+	}
 
 	cfg, err := config.Load(getEnvConfigPath(CLI.ConfigPath))
 	if err != nil {
@@ -46,4 +53,32 @@ func getEnvConfigPath(configPath string) string {
 		configPath = "config.yml"
 	}
 	return configPath
+}
+
+func showVersion() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Println("Build information not available")
+		return
+	}
+
+	version := info.Main.Version
+	if version == "(devel)" {
+		var commit string
+		var dirty bool
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				commit = setting.Value
+			}
+			if setting.Key == "vcs.modified" {
+				dirty = setting.Value == "true"
+			}
+		}
+		version += " " + commit
+		if dirty {
+			version += " dirty"
+		}
+	}
+
+	fmt.Printf("%s\n", version)
 }
