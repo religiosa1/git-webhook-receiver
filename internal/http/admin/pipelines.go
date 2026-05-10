cf07ed318d365dfd598ae0e1ccb07aaa8fb2037e
Viacheslav Azarov <public@religiosa.ru>
2026-05-10 17:39:24 +0200

    simplistic pileine display

--- internal/http/admin/pipelines.go
+++ internal/http/admin/pipelines.go
@@ -17,7 +17,37 @@ type ListPipelines struct {
 
 func (s ListPipelines) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	logger := middleware.GetLogger(req.Context())
-	if err := views.ToDo("Pipelines list").Render(req.Context(), w); err != nil {
+	queryParams := req.URL.Query()
+	query := actionsdb.ListPipelineRecordsQuery{
+		Project:    queryParams.Get("project"),
+		DeliveryID: queryParams.Get("deliveryId"),
+		Cursor:     queryParams.Get("cursor"),
+	}
+	var err error
+	query.Status, err = actionsdb.ParsePipelineStatus(queryParams.Get("status"))
+	if err != nil {
+		logger.Warn("Error parsing pipeline state", slog.Any("error", err))
+		// just logging out, no execution abort here
+	}
+
+	page, err := s.DB.ListPipelineRecords(query)
+	if err != nil {
+		statusCode := 500
+		if errors.Is(err, actionsdb.ErrBadCursor) || errors.Is(err, actionsdb.ErrCursorAndOffset) {
+			statusCode = 400
+		}
+		w.WriteHeader(statusCode)
+		// TODO:
+		requestID := middleware.GetRequestID(req.Context())
+		if writeErr := views.InternalError(requestID).Render(req.Context(), w); writeErr != nil {
+			logger.Error("error while writing error response", slog.Any("error", writeErr))
+		}
+	}
+
+	viewModel := views.PipelinesListViewModel{
+		Page: page,
+	}
+	if err := views.PipelinesList(viewModel).Render(req.Context(), w); err != nil {
 		logger.Error("Error while writing response", slog.Any("error", err))
 	}
 }
@@ -29,7 +59,7 @@ type GetPipeline struct {
 func (s GetPipeline) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	pipeID := req.PathValue("pipeId")
 	logger := middleware.GetLogger(req.Context()).With(slog.String("pipe_id", pipeID))
-	_, err := s.DB.GetPipelineRecord(pipeID)
+	record, err := s.DB.GetPipelineRecord(pipeID)
 	if errors.Is(err, sql.ErrNoRows) {
 		w.WriteHeader(404)
 		if writeErr := views.NotFound().Render(req.Context(), w); writeErr != nil {
@@ -45,7 +75,10 @@ func (s GetPipeline) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		}
 		return
 	}
-	if err := views.ToDo("Pipeline").Render(req.Context(), w); err != nil {
+	viewModel := views.PipelineItemViewModel{
+		Record: record,
+	}
+	if err := views.PipelineItem(viewModel).Render(req.Context(), w); err != nil {
 		logger.Error("Error while writing response", slog.Any("error", err))
 	}
 }
